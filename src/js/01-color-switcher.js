7cda023432a1ddc89cbcedba9d923fe544bb50e5
IvanImen <vanyagarret@gmail.com>
2023-11-01 04:06:10 +0200

    switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const el = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+  background: document.querySelector('body'),
+};
 
+let timerId;
+
+el.buttonStop.disabled = true;
+
+el.buttonStart.addEventListener('click', startChangeColor.bind(el));
+el.buttonStop.addEventListener('click', stopChangeColor.bind(el));
+
+function startChangeColor(event) {
+  event.currentTarget.disabled = true;
+  this.buttonStop.disabled = false;
+
+  timerId = setInterval(changeColor, 1000);
+}
+
+function stopChangeColor(event) {
+  event.currentTarget.disabled = true;
+  this.buttonStart.disabled = false;
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColor() {
+  const color = getRandomHexColor();
+  el.background.style.backgroundColor = color;
+}
